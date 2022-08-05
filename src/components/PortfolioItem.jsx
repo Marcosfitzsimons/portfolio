563ca0a558a7fded52e8540559a822611542b0ba
marcosfitzsimons <marcosfitzsimons@gmail.com>
2022-08-05 10:40:25 -0300

    update-portfolio-links-design

--- src/components/PortfolioItem.jsx
+++ src/components/PortfolioItem.jsx
@@ -22,9 +22,9 @@ const PortfolioItem = ({ projectImage, liveSiteUrl, repositoryUrl }) => {
   return (
     <article
       onClick={clickProject}
-      className="portfolio-item py-4 mb-6 lg:mb-8 max-w-[290px] m-auto relative"
+      className="portfolio-item py-4 mb-[4rem] max-w-[290px] m-auto relative"
     >
-      <div className="portfolio-item-inner border-[4px] border-accent/60 rounded-xl overflow-hidden shadow-xl shadow-white/10">
+      <div className="portfolio-item-inner border-[4px] border-accent/60 rounded-xl overflow-hidden">
         <div className="portfolio-img-container">
           <img
             src={projectImage}
@@ -33,26 +33,26 @@ const PortfolioItem = ({ projectImage, liveSiteUrl, repositoryUrl }) => {
           />
         </div>
 
-        <div className="portfolio-links-container flex bottom-[-1.5rem] left-0 absolute z-[9] gap-2 justify-center items-center w-full">
-          <div className="hover:bg-accent/30 hover:border hover:border-neutral animate__animated animate__fadeIn portfolio-links-item p-2 bg-accent/10 shadow-md shadow-white/30 rounded-md flex items-center justify-center border border-accent">
+        <div className="portfolio-links-container flex bottom-[-2.2rem] left-0 absolute z-[9] gap-2 justify-center items-center w-full">
+          <div className="cursor-pointer hover:bg-accent/20 hover:border hover:border-accent/70 hover:border-t-transparent animate__animated animate__fadeIn portfolio-links-item p-2 bg-accent/10 shadow-md shadow-white/30 rounded-md flex items-center justify-center border border-accent border-t-transparent rounded-t-none">
             <a
               href={liveSiteUrl}
               target="_blank"
               rel="noreferrer"
-              className=" select-none font-medium text-center flex flex-col gap-2 items-center justify-between text-neutral w-[5.3rem] h-[4.4rem] lg:w-[4.4rem] lg:h-[4.4rem] pt-2"
+              className="select-none font-medium text-center text-sm flex items-center justify-center gap-1 text-neutral w-[5.9rem] h-[2rem]"
             >
-              Live site <MdWeb className="text-neutral text-3xl" />
+              Live site <MdWeb className="text-neutral text-xl" />
             </a>
           </div>
-          <div className="hover:bg-accent/30 hover:border hover:border-neutral animate__animated animate__fadeIn portfolio-links-item p-2 bg-accent/10 shadow-md shadow-white/30 rounded-md flex items-center justify-center border border-accent/70">
+          <div className="cursor-pointer hover:bg-accent/20 hover:border hover:border-accent/70 hover:border-t-transparent animate__animated animate__fadeIn portfolio-links-item p-2 bg-accent/10 shadow-md shadow-white/30 rounded-md flex items-center justify-center border border-accent border-t-transparent rounded-t-none">
             <a
               href={repositoryUrl}
               target="_blank"
               rel="noreferrer"
-              className=" select-none font-medium text-center flex flex-col items-center leading-4 justify-between text-neutral w-[5.3rem] h-[4.3rem] lg:w-[4.4rem] lg:h-[4.4rem]"
+              className="select-none font-medium text-center text-sm flex items-center justify-around leading-4 text-neutral w-[5.9rem] h-[2rem]"
             >
               Github repository{" "}
-              <RiGithubFill className="text-neutral text-3xl" />
+              <RiGithubFill className="text-neutral text-4xl" />
             </a>
           </div>
         </div>
