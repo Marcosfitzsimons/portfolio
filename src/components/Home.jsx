ea725e940a989a51119ec8243239a10a27637792
marcosfitzsimons <marcosfitzsimons@gmail.com>
2022-11-18 16:46:31 -0300

    Change mobile design

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import HomeIllustration from "./HomeIllustration";
 import Typed from "typed.js";
 import { Link, animateScroll as scroll } from "react-scroll";
 import { motion } from "framer-motion";
@@ -7,30 +6,18 @@ import { motion } from "framer-motion";
 const homeVariants = {
   hidden: {
     opacity: 0,
-    x: "-100vw",
+    y: 50,
+    scale: 0.95,
   },
   visible: {
     opacity: 1,
-    x: 0,
+    y: 0,
+    scale: 1,
     transition: {
-      type: "spring",
-      mass: 0.6,
-      damping: 12,
-      when: "beforeChildren",
-      staggerChildren: 0.4,
+      duration: 0.4,
     },
   },
 };
-
-const childVariants = {
-  hidden: {
-    opacity: 0,
-  },
-  visible: {
-    opacity: 1,
-  },
-};
-
 const Home = () => {
   const el = React.useRef(null);
   // Create reference to store the Typed instance itself
@@ -64,20 +51,14 @@ const Home = () => {
     >
       <div className="container">
         <div className="row flex flex-wrap -mx-4 relative">
-          <div className="home__info max-w-full lg:max-w-[60%] basis-full lg:basis-[60%] grow-0 shrink-0 px-4">
-            <h1
-              className="home__title text-3xl lg:text-4xl my-5 mx-0 font-bold"
-              variants={childVariants}
-            >
+          <div className="home__info max-w-full basis-full grow-0 shrink-0 px-4 text-center lg:basis-[90%] lg:text-start">
+            <h1 className="home__title flex flex-col items-center gap-2 text-3xl lg:flex-row lg:text-4xl my-5 mx-0 font-bold">
               Hello, my name is{" "}
               <span className="font-secondary-font font-bold text-accent">
                 Marcos Fitzsimons
               </span>
             </h1>
-            <motion.h2
-              className="font-bold home__profession text-[1.3rem] lg:text-4xl my-5 mx-0"
-              variants={childVariants}
-            >
+            <motion.h2 className="font-bold home__profession text-[1.3rem] lg:text-4xl my-5 mx-0">
               I'm a{" "}
               <span
                 className="typing text-accent"
@@ -85,26 +66,22 @@ const Home = () => {
                 ref={el}
               />
             </motion.h2>
-            <motion.p
-              className="home__description mb-[70px] text-lg lg:text-xl 2xl:w-[80%] text-base-100"
-              variants={childVariants}
-            >
+            <motion.p className="home__description mb-[70px] text-lg text-base-100 max-w-[85%] mx-auto lg:mx-0 lg:max-w-[65%] lg:text-xl 2xl:max-w-[55%]">
               I'm a self-taught developer who enjoys creating beautiful,
               friendly and functional user interfaces.
             </motion.p>
-            <motion.div className="" variants={childVariants}>
+            <motion.div className="">
               <Link
                 to="about"
                 smooth={true}
                 offset={-20}
-                duration={500}
+                duration={800}
                 className="btn-hover btn shadow-md shadow-neutral/10 overflow-hidden bg-transparent border-accent/80 rounded-md hire-me w-[9rem] text-neutral z-20 relative hover:bg-transparent hover:border-accent/80"
               >
                 <span>About me</span>
               </Link>
             </motion.div>
           </div>
-          <HomeIllustration childVariants={childVariants} />
         </div>
       </div>
     </motion.section>
