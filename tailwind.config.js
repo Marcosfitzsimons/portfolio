b359e35ecdee0719a0707f366e63e8d75d46978c
marcosfitzsimons <marcosfitzsimons@gmail.com>
2022-11-25 14:22:05 -0300

    Change primary and secondary colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,29 +14,29 @@ module.exports = {
     themes: [
       {
         default: {
-          "primary": "#151515", // veryDarkGray
-          "secondary": "#222222", // darkGray
+          "primary": "#0e1217", // veryDarkGray
+          "secondary": "#171b22", // darkGray
           "accent": "#0d9488", // themeColor
           "neutral": "#f8fffe", // veryLightGray
           "base-100": "#e5e0df", // lightGray
         },
         pink: {
-          "primary": "#151515", // veryDarkGray
-          "secondary": "#222222", // darkGray
+          "primary": "#0e1217", // veryDarkGray
+          "secondary": "#171b22", // darkGray
           "accent": "#db2777", // themeColor
           "neutral": "#f8fffe", // veryLightGray
           "base-100": "#e5e0df", // lightGray
         },
         indigo: {
-          "primary": "#151515", // veryDarkGray
-          "secondary": "#222222", // darkGray
+          "primary": "#0e1217", // veryDarkGray
+          "secondary": "#171b22", // darkGray
           "accent": "#818cf8", // themeColor
           "neutral": "#f8fffe", // veryLightGray
           "base-100": "#e5e0df", // lightGray df4996
         },
         yellow: {
-          "primary": "#151515", // veryDarkGray
-          "secondary": "#222222", // darkGray
+          "primary": "#0e1217", // veryDarkGray
+          "secondary": "#171b22", // darkGray
           "accent": "#eab308",
           "neutral": "#f8fffe", // veryLightGray
           "base-100": "#e5e0df", // lightGray df4996
