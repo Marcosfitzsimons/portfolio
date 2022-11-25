70db48a6ddc3faa4567026a430d126b5c41d37c4
marcosfitzsimons <marcosfitzsimons@gmail.com>
2022-11-25 14:22:43 -0300

    Refactor code and change design

--- src/components/ThemeChange.jsx
+++ src/components/ThemeChange.jsx
@@ -1,68 +1,74 @@
 import React from "react";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import { themeChange } from "theme-change";
 import { FaPalette } from "react-icons/fa";
-import "animate.css";
 
 const ThemeChange = () => {
+  const [isShown, setIsShown] = useState(false);
+
+  const handleIsShown = () => {
+    setIsShown((prevValue) => !prevValue);
+  };
+
   useEffect(() => {
     themeChange(false);
   }, []);
 
-  const showThemeSwitcher = () => {
-    const themeSwitcher = document.querySelector(".theme-switcher");
-    themeSwitcher.classList.toggle("active");
-  };
+  const colors = [
+    {
+      id: 1,
+      badgeColor: "#0d9488",
+      dataSetTheme: "default",
+      text: "emerald",
+    },
+    {
+      id: 2,
+      badgeColor: "#df4996",
+      dataSetTheme: "pink",
+      text: "pink",
+    },
+    {
+      id: 3,
+      badgeColor: "#818cf8",
+      dataSetTheme: "indigo",
+      text: "indigo",
+    },
+    {
+      id: 4,
+      badgeColor: "#EAB308",
+      dataSetTheme: "yellow",
+      text: "gold",
+    },
+  ];
 
   return (
     <div
-      onClick={showThemeSwitcher}
-      className="theme-switcher w-[10rem] fixed right-[-9.5rem] top-8 rounded-lg shadow-md shadow-accent/10 cursor-pointer transition-all z-50"
+      onClick={handleIsShown}
+      className={`${
+        isShown ? "-right-1" : "right-[-9.5rem]"
+      } w-[10rem] fixed top-8 rounded-lg rounded-tl-none cursor-pointer transition-all z-50 border border-zinc-600`}
     >
-      <div className="open-theme-switcher shadow-md shadow-neutral/20 w-10 h-10 rounded-md rounded-tr-none rounded-br-none left-[-2.5rem] top-0 z-40 bg-red absolute flex justify-center items-center">
+      <div className="open-theme-switcher w-10 h-10 rounded-md rounded-tr-none rounded-br-none left-[-2.5rem] -top-[1px] z-40 bg-red absolute flex justify-center items-center">
         <FaPalette className="text-neutral/80" />
       </div>
-      <ul className="flex flex-col w-full border-2 border-secondary/40 border-t-0 p-3 rounded-bl-lg bg-primary z-50">
-        <li className="flex items-center gap-[.4rem] hover:scale-105 transition-transform">
-          <div className="badge badge-lg bg-[#0d9488] border-secondary z-20"></div>
-          <button
-            data-set-theme="default"
-            data-act-class="ACTIVECLASS"
-            className="btn btn-ghost w-full relative right-8 z-30 text-neutral hover:bg-transparent"
+      <ul className="flex flex-col w-full py-3 px-1 pr-2 rounded-bl-lg bg-secondary z-50">
+        {colors.map((color) => (
+          <li
+            className="flex items-center gap-3 text-slate-300 hover:text-white p-2 rounded-lg hover:bg-zinc-700/60"
+            key={color.id}
           >
-            emerald
-          </button>
-        </li>
-        <li className="flex items-center gap-1 hover:scale-105 transition-transform">
-          <div className="badge badge-lg bg-[#df4996] border-secondary z-20"></div>
-          <button
-            data-set-theme="pink"
-            data-act-class="ACTIVECLASS"
-            className="btn btn-ghost w-full relative right-[2.88rem] z-30 text-neutral hover:bg-transparent"
-          >
-            pink
-          </button>
-        </li>
-        <li className="flex items-center gap-[.4rem] hover:scale-105 transition-transform">
-          <div className="badge badge-lg bg-[#818cf8] border-secondary z-20"></div>
-          <button
-            data-set-theme="indigo"
-            data-act-class="ACTIVECLASS"
-            className="btn btn-ghost w-full relative right-[2.41rem] z-30 text-neutral hover:bg-transparent"
-          >
-            indigo
-          </button>
-        </li>
-        <li className="flex items-center gap-[.4rem] hover:scale-105 transition-transform">
-          <div className="badge badge-lg bg-[#EAB308] border-secondary z-20"></div>
-          <button
-            data-set-theme="yellow"
-            data-act-class="ACTIVECLASS"
-            className="btn btn-ghost w-full relative right-[2.85rem] z-30 text-neutral hover:bg-transparent"
-          >
-            gold
-          </button>
-        </li>
+            <div
+              className={`badge badge-lg bg-[${color.badgeColor}] border-secondary`}
+            ></div>
+            <button
+              data-set-theme={color.dataSetTheme}
+              data-act-class="ACTIVECLASS"
+              className="w-full text-start uppercase tracking-wide"
+            >
+              {color.text}
+            </button>
+          </li>
+        ))}
       </ul>
     </div>
   );
